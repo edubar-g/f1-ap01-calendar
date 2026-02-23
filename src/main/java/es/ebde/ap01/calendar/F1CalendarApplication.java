0110cebbbc7d2469501fceb604aeceaa1ddcdb15
Eduardo Lunar Baricellis <eduardo.lunar@nter.es>
2026-02-23 14:02:08 +0100

    Initial Commit

---
+++ src/main/java/es/ebde/ap01/calendar/F1CalendarApplication.java
@@ -0,0 +1,13 @@
+package es.ebde.ap01.calendar;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class F1CalendarApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(F1CalendarApplication.class, args);
+	}
+
+}
---
+++ src/test/java/es/ebde/ap01/calendar/F1CalendarApplicationTests.java
@@ -0,0 +1,13 @@
+package es.ebde.ap01.calendar;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class F1CalendarApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
